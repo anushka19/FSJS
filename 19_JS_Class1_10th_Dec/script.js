7f0d71f092700c2a1b8a9aaa448330f9d9c8129c
ANUSHKA BOMMAKANTY <anushkabommakanty@gmail.com>
2022-12-11 22:40:20 +0530

    JS Basics

---
+++ 19_JS_Class1_10th_Dec/script.js
@@ -0,0 +1,84 @@
+// 7;
+// 2.5;
+ //number
+
+// ("anushka") ;
+// ('anushka');
+ //String - any qoutes used
+
+//  true;
+//  false;
+//Boolean value - Gorge boole is know as father of Boolean Algebra
+
+// null;
+// undefined;
+//Empty values
+
+//primitive values or primitive datatype all 5 above 
+
+//Array and object
+//[1,2,3,4,5,6,7,"anushka",'anu',true] //any type can be added
+//0
+//index - at what positions values are kept
+
+//{
+   //key :value
+   //name:'anushka',
+
+//}
+
+// Non primitive - more than one value
+
+//Variable - place holder of value
+
+//var , let and const 
+//var y = 25; - why not to use var
+
+//let x = 25;
+const z = '45673563829'; //constant
+
+let balance = 10000000000
+
+let firstname ='Anushka'
+let lastname = 'Bommakanty'
+let mobileno = '5446789'
+console.log("My first name is" ,firstname );
+console.log("My last name is" + lastname);
+console.log(mobileno);
+//Template Literal
+
+console.log(`My current first name is ${firstname} ${lastname} ${mobileno}`);
+
+//History
+//Basic pillar of a programming lanuage
+//Values
+//Data types
+//Difference between primitive and non primitive datatype
+//console.log => how to print something on terminal
+//declaration variable
+//template literal
+
+//Operator
+
+let x = 10;
+let y = 10;
+
+//Assigmnmet operatorr
+
+//Arithmentic operator
+
+//Comparison operator
+
+
+//Decison or Conditions
+
+let age = 16;
+//if condition is true
+if (age == 18){
+    console.log('You are allowed inside the park');
+}
+else{
+    console.log('Out of the park')
+}
+
+//if- else, if-else if-else
