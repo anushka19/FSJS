2eeb44d3bc6696b8b72eb25bada47d3abae79b54
ANUSHKA BOMMAKANTY <anushkabommakanty@gmail.com>
2023-03-06 00:16:38 +0530

    react hooks - useeffects

--- 6-React-Lectures/Practice/hooks-effects/src/App.js
+++ 6-React-Lectures/Practice/hooks-effects/src/App.js
@@ -2,11 +2,35 @@ import React, { useState, useEffect } from 'react'
 
 export default function App(){
   const [resourceType,setResourceType] = useState('posts')
+  //const [windowWidth,setwindowWidth] = useState(window.innerWidth)
+  //const [items,setItems] = useState([])
+  //console.log('render'); - to check how many times render runs
   //useeffects render everytime
   useEffect(() => {
-    console.log("render")
-  })
+    console.log("resource changed")
+
+    return () =>  {
+      console.log('return from resource change');
+    }//it will first run the return code then the react code, to clean up the prev things
+    // fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    //   .then(response => response.json())
+    //   .then(json => setItems(json))
+   
+  },[resourceType]) //whenever teh resource type changes it will render again, when the ddefault type is changed then only to render
+  // const handleResize = () => {
+  //   setwindowWidth(windowWidth.innerWidth)
+  // }
+  // useEffect( () =>{
+  //   window.addEventListener('resize', handleResize)
+
+  //   return () =>{
+  //     window.removeEventListener('resize',handleResize)
+  //   } 
+  // },[])
   return (
+    // <div>{windowWidth}
+
+    // </div>
     <>
     <div>
       <button onClick={() => setResourceType('posts')}>Posts</button>
@@ -14,8 +38,12 @@ export default function App(){
       <button onClick={() => setResourceType('comments')}>Comments</button>
     </div>
     <h1>{resourceType}</h1>
+    {/*items.map(item =>   {
+      return <pre>{JSON.stringify(item)} </pre>
+    })} */}
     </>
   );
 
-}
+  }
 
+//state change, props change
