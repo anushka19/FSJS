e3d590635b9d07dc2cbd30b67783331ce392792b
ANUSHKA BOMMAKANTY <anushkabommakanty@gmail.com>
2023-06-07 23:41:42 +0530

    react lecs

---
+++ 6-React-Lectures/25-React_CDN_12th-Feb/Practice2-7th-June/script.js
@@ -0,0 +1,31 @@
+const name = <h1>Anushka Bommakanty</h1>; //jsx ement
+
+//jsx
+
+
+const name2 ="Anushka"; //JS
+
+<p>Anu</p> //HTML
+//when we combine it is JSX
+
+const header = 
+<div>
+
+    <h1>Hello</h1>
+    <p>Hello Namaste</p>
+
+</div>
+
+
+
+//JSX Element - can have only 1 tag, 1 parent tag and enclose inside it. so enclose in single tage to it becomes JSX element
+
+//why one div - react fragments
+
+const header2 = 
+<>
+
+    <h1>Hello</h1>
+    <p>Hello Namaste</p>
+
+</>
