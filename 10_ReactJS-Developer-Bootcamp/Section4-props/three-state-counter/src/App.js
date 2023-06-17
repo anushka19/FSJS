299053cf70ec3f5a0372579a7371a2e9778971b1
ANUSHKA BOMMAKANTY <anushkabommakanty@gmail.com>
2023-06-18 01:37:47 +0530

    react tic tac toe app

---
+++ 10_ReactJS-Developer-Bootcamp/Section4-props/three-state-counter/src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount]= useState(0)
+  
+
+
+  return(
+    <div className='App'>
+      <header>
+        <h1>Counter App using State/Hooks</h1>
+      </header>
+      <h2>Current Value of count is {count}</h2>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => count > 10 ? "" : setCount(count + 1)}>Increase Counter</button>
+      <button onClick={() => count < 1 ? "" : setCount(count - 1)}>Decrease Counter</button>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ 10_ReactJS-Developer-Bootcamp/Section5-Tic-Tac-Toe/tic-tac-toe-app/src/Components/Icons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Icons =({name}) =>{
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+export default Icons
+//5:51 2nd video 
