b366a6bc4a8711d29f19e6ed175fceaf05f29a9c
ANUSHKA BOMMAKANTY <anushkabommakanty@gmail.com>
2023-06-21 22:22:42 +0530

    context api

---
+++ 10_ReactJS-Developer-Bootcamp-by-hitesh-sir/Section6-contextApi/side-one-context/src/problem.js
@@ -0,0 +1,33 @@
+// import React, {useState} from "react"
+// import './App.css';
+
+// //prop drilling
+// const GrandChild = ({brand}) =>{
+//   return(
+//     <div>
+//       <h3>Grandchild: </h3>
+//       <Child brand={brand}/>
+//     </div>
+//   )
+// }
+
+// const Child = ({brand}) =>{
+//   return(
+//     <h2>Child: {brand}</h2>
+//   )
+// }
+
+
+
+// function App() {
+//   const [brandname] = useState("Amazon") // if changing any value then only set
+//   return (
+//     <div> 
+//       <h1>Hello</h1>
+     
+//       <GrandChild brand={brandname}/>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ 10_ReactJS-Developer-Bootcamp-by-hitesh-sir/Section6-contextApi/side-one-context/src/provider.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+import  PackageContext from "./context"
+
+
+const Provider = props => {
+    const [mission, setMission] = useState({
+        mname : "Go to Russia",
+        agent: 7,
+        accept: "Not Accepted"
+    })
+    return(
+        <PackageContext.Provider 
+        value={{
+            data: mission,
+            isMissionAccepted: () => {
+                setMission({...mission, accept: "ACCEPTED"})
+            }
+        }}
+        >
+            {props.children}
+        </PackageContext.Provider>
+    )
+}
